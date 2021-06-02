2ce980afc2174a230c6be75a9683561152c079e6
justinhchae <68864597+justinhchae@users.noreply.github.com>
2021-06-02 11:53:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src import utils, bert_sequence
 import torch
+from pprint import pprint
 
 # sets global path to torch.hub cache (for download and recall)
 torch.hub.set_dir(utils.constants.CACHE_PATH)
@@ -10,14 +11,14 @@ if __name__ == '__main__':
     device = utils.config.run()
 
     torch_corpora_names = ["ag_news", 'dbpedia']
-    
+
     results = []
     # run the network for the bert sequence classification model
     for torch_corpora_name in torch_corpora_names:
         result = bert_sequence.run.network(device, torch_corpora_name=torch_corpora_name)
         results.append(result)
 
-    print(results)
+    pprint(results)
 
     # uncomment below to run the jiant validator script
     # util.validate_jiant()
