409edb541c1ae80ae0d2e85cee164c7e49a6a164
justinhchae <68864597+justinhchae@users.noreply.github.com>
2021-06-02 13:25:37 -0400

    update results output

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src import utils, bert_sequence
 import torch
 from pprint import pprint
+import pandas as pd
+import os
 
 # sets global path to torch.hub cache (for download and recall)
 torch.hub.set_dir(utils.constants.CACHE_PATH)
@@ -16,11 +18,15 @@ if __name__ == '__main__':
     # run the network for the bert sequence classification model
     for torch_corpora_name in torch_corpora_names:
         # do_break_testing: if true, runs 3 cycles in each epoch and cuts training short for debugging
-        result = bert_sequence.run.network(device, torch_corpora_name=torch_corpora_name, do_break_testing=False)
+        result = bert_sequence.run.network(device, torch_corpora_name=torch_corpora_name, do_break_testing=True)
         results.append(result)
 
-    # TODO: Read into dataframe and graph results
-    pprint(results)
+    # TODO: plot results in graph
+    print("=" * 20, "Final Total Experiment Summary", "=" * 20)
+    df = pd.concat(pd.DataFrame(i) for i in results)
+    print(df)
+    results_filepath = os.sep.join([utils.constants.DATA_PATH, 'results.csv'])
+    df.to_csv(results_filepath, index=False)
 
     # uncomment below to run the jiant validator script
     # util.validate_jiant()
