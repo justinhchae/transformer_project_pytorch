9b3b65f2091c6f9ac163e4273bbf46b67fce5b27
justinhchae <68864597+justinhchae@users.noreply.github.com>
2021-06-14 11:43:05 -0400

    Update constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,6 +1,6 @@
 import os
 
-WORKFLOW_ROOT = os.environ['PWD']
+WORKFLOW_ROOT = os.getcwd()
 DATA_PATH = os.path.join(WORKFLOW_ROOT, 'data')
 CACHE_PATH = os.path.join(WORKFLOW_ROOT, 'cache')
 FIG_PATH = os.path.join(WORKFLOW_ROOT, 'figures')
